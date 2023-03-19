2d72b995d411308237c80f8720ada9d98ee15c07
Theop1234 <121819369+Theop1234@users.noreply.github.com>
2023-03-19 19:43:48 -0400

    Update decrypt.pl

--- file-encryptor/decrypt.pl
+++ file-encryptor/decrypt.pl
@@ -2,31 +2,42 @@ use strict;
 use warnings;
 use Crypt::CBC;
 
-print "Enter password:\t";
+# Get password from user
+print "Enter password: ";
 my $password = <STDIN>;
 chomp $password;
 
+# Initialize decryption cipher
 my $cipher = Crypt::CBC->new(
     -key    => $password,
     -cipher => 'Blowfish',
 );
 
-my @files = grep { /\.enc$/ } glob("*");
-
+# Decrypt each file in current directory
+my @files = grep { -f $_ } glob("*");
 foreach my $file (@files) {
+    next if $file =~ /\.enc$/;   # Skip encrypted files
+    next unless -f $file;        # Skip non-files
+    next unless -r $file;        # Skip unreadable files
+    next if -d $file;            # Skip directories
+    my $enc_file = $file.'.enc';
+    next unless -e $enc_file;    # Skip files with no corresponding .enc file
+
     # Read file contents
-    open my $fh, '<', $file or die "Can't open $file: $!";
+    open my $fh, '<', $enc_file or die "Can't open $enc_file: $!";
     my $ciphertext = do { local $/; <$fh> };
     close $fh;
 
+    # Decrypt file contents
     my $plaintext = $cipher->decrypt($ciphertext);
 
-    $file =~ s/\.enc$//;
+    # Write decrypted file
     open $fh, '>', $file or die "Can't write to $file: $!";
     print $fh $plaintext;
     close $fh;
 
-    unlink $file.'.enc' or die "Can't remove $file.enc: $!";
+    # Remove encrypted file
+    unlink $enc_file or die "Can't remove $enc_file: $!";
 }
 
 print "Decryption complete.\n";
